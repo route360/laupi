660aefc051193f90739a01acb80ad69a0d1e40b0
Jan Silbersiepe <jan.silbersiepe@student.hpi.de>
2015-07-24 10:34:29 +0200

    fixed cfg defaults

--- app/scripts/cfg.default.js
+++ app/scripts/cfg.default.js
@@ -16,6 +16,5 @@ r360.config.serviceUrl = 'http://api.route360.net/api_brandenburg/';
 r360.config.serviceKey = '<<API KEY>>';
 
 // der tolerance Wert sollte immer geringer sein als der StrokeWidth Wert
-r360.config.defaultPolygonLayerOptions.strokeWidth = 15;
-r360.config.defaultPolygonLayerOptions.tolerance = 10;
-r360.config.defaultPolygonLayerOptions.offset = 100;
+r360.config.defaultPolygonLayerOptions.strokeWidth = 20
+r360.config.defaultPolygonLayerOptions.tolerance = 13;
